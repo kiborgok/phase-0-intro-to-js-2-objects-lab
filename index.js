8d0d5977317eed0acd3095f49bfe81cc164955f4
Alex Kiborgok <borgokalek14@gmail.com>
2022-04-08 11:14:43 +0300

    create functions to manipulate objects

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
-// Write your solution in this file!
+const employee = {
+    name: "Alex",
+    streetAddress: "Nairobi 00100"
+}
+
+function updateEmployeeWithKeyAndValue(employee, key, value) {
+    return { ...employee, [key]: value  };
+}
+
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value) {
+    employee[key] = value
+    return employee
+}
+
+function deleteFromEmployeeByKey(employee, key) {
+    let copyEmployee = {...employee}
+    delete copyEmployee[key]
+    return copyEmployee
+}
+
+function destructivelyDeleteFromEmployeeByKey(employee, key) {
+    delete employee[key];
+    return employee;
+}
